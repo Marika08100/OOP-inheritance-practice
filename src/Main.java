c2da93105325cc9d873a1c90e8fcd4e271c2785e
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-10 16:47:10 +0300

    is modified toString

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ import data.Student;
 
 public class Main {
     public static void main(String[] args) {
-        // Ezt nyugodtan módosítsd!
         Person person = new Person("Aurelia Montague" , "123 Main St, NY, USA");
         Staff staff = new Staff("Aurelia Montague", "123 Main St, NY, USA", "Avalon Academy", 1750);
         Student student = new Student("Aurelia Montague", "123 Main St, NY, USA", "Roxfort", 2023, 115);
