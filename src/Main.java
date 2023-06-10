eede3b5153274318b7266c00418f3a6422dbe871
Marika08100 <132154696+Marika08100@users.noreply.github.com>
2023-06-10 16:35:02 +0300

    is rdy

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 // Importáljuk a package-ekbe rendezett classokat
+
 import data.Person;
 import data.Staff;
 import data.Student;
@@ -6,8 +7,12 @@ import data.Student;
 public class Main {
     public static void main(String[] args) {
         // Ezt nyugodtan módosítsd!
-        Person person = new Person();
-        Staff staff = new Staff();
-        Student student = new Student();
+        Person person = new Person("Aurelia Montague" , "123 Main St, NY, USA");
+        Staff staff = new Staff("Aurelia Montague", "123 Main St, NY, USA", "Avalon Academy", 1750);
+        Student student = new Student("MArika", "Csik", "Progmatic", 2023, 115);
+
+        System.out.println(person.toString());
+        System.out.println(staff.toString());
+        System.out.println(student.toString());
     }
 }
